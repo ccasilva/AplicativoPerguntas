61f4536621b962d6bee26de7a11243cc0f8d1e5f
Carlos <als04545@gmail.com>
2020-03-30 21:00:46 -0300

    Start Commit

---
+++ lib/main.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:projetoperguntas/questionario.dart';
+import 'package:projetoperguntas/resultado.dart';
+import './questao.dart';
+import './resposta.dart';
+
+main() => runApp(PerguntaApp());
+
+class PerguntaApp extends StatefulWidget{
+
+  @override
+  _PerguntaAppState createState() => _PerguntaAppState();
+}
+
+class _PerguntaAppState extends State<PerguntaApp> {
+  var _perguntaSelecionada = 0;
+  var _pontuacaoTotal = 0;
+
+  final _perguntas = const [
+    {
+      'texto': 'Qual é a sua cor favorita?',
+      'respostas': [
+         {'texto':'Preto', 'pontuacao': 10},
+         {'texto':'Vermelho', 'pontuacao': 5},
+         {'texto':'Verde', 'pontuacao': 3},
+         {'texto':'Branco', 'pontuacao': 1},
+      ]
+    },
+    {
+      'texto': 'Qual é o seu animal favorito?',
+      'respostas': [
+         {'texto':'Coelho', 'pontuacao': 10},
+         {'texto':'Cobra', 'pontuacao': 5},
+         {'texto':'Elefante', 'pontuacao': 3},
+         {'texto':'Leão', 'pontuacao': 1}
+       ]
+    },
+    {
+      'texto': 'Qual é o seu instrutor favorito?',
+      'respostas': [
+          {'texto':'Maria', 'pontuacao': 10},
+          {'texto':'João', 'pontuacao': 5},
+          {'texto':'Léo', 'pontuacao': 3},
+          {'texto':'Pedro', 'pontuacao': 1}
+       ]
+    }
+  ];
+
+  void _responder(int pontuacao){
+    if(temPerguntaSelecionada) {
+      setState(() {
+        _perguntaSelecionada++;
+        _pontuacaoTotal += pontuacao;
+      });
+    }
+  }
+
+  void _reiniciarQuestionario(){
+    setState(() {
+      _perguntaSelecionada = 0;
+      _pontuacaoTotal = 0;
+    });
+
+  }
+
+
+  bool get temPerguntaSelecionada{
+    return _perguntaSelecionada < _perguntas.length;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    // TODO: implement build
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Perguntas'),
+        ),
+        body: temPerguntaSelecionada
+            ? Questionario(
+                 perguntas: _perguntas,
+                 perguntaSelecionada: _perguntaSelecionada,
+                 quandoResponder: _responder,
+              )
+            : Resultado(_pontuacaoTotal, _reiniciarQuestionario),
+      )
+    );
+  }
+}
